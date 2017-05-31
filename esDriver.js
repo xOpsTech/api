09d984e161f35d047c2b2db4013db935fa52b63f
evantha <VMANIEV@PEROOT.COM>
2017-05-31 23:27:48 +0530

    alert update endpoint

--- esDriver.js
+++ esDriver.js
@@ -11,13 +11,24 @@ var _index_data = function (_index, _type, _id, message) {
   });
 };
 
+var _update_data = function (_index, _type, _id, message) {
+  client.update({
+    index: _index,
+    type: _type,
+    id: _id,
+    body: message
+  }, function (error, response) {
+    console.log(error || response);
+  });
+};
+
 var _read_data = function (_index, _type, query, callback) {
   client.search({
     index: _index,
     type: _type,
     body: query
   }).then(function (resp) {
-    var hits = resp.hits.hits;        
+    var hits = resp.hits.hits;
     callback(hits);
   }, function (err) {
     console.trace(err.message);
@@ -32,22 +43,29 @@ module.exports = {
     _index_data('user', 'project', userId, projectObj);
   },
   medianMetric: function (callback) {
-	console.log('reading median metrics');
+    console.log('reading median metrics');
     var query = { query: { range: { storedTimestamp: { gte: "now-1h" } } }, size: 100 };
-     _read_data('wpt', 'median_metrics', query, callback);
-    
+    _read_data('wpt', 'median_metrics', query, callback);
+
   },
   allAlerts: function (callback) {
-	console.log('reading alerts');
-    var query = { query: { match_all: {} }, size: 100};
-     _read_data('live_alert_index', 'alert', query, callback);
-    
+    console.log('reading alerts');
+    var query = { query: { match_all: {} }, size: 100 };
+    _read_data('live_alert_index', 'alert', query, callback);
+
   },
   saveAlerts: function (alertObj) {
-	console.log('save alerts');
+    console.log('save alerts');
     var alertId = alertObj.eventId;
-	console.log(alertId);
+    console.log(alertId);
     _index_data('live_alert_index', 'alert', alertId, alertObj);
-    
+
+  },
+  updateAlerts: function (alertObj) {
+    console.log('update alerts');
+    var alertId = alertObj.doc.eventId;
+    console.log(alertId);
+    _update_data('live_alert_index', 'alert', alertId, alertObj);
+
   }
 }
