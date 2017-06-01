093d601648e95278862e26316390982eeea2a1fa
Shashith Darshana <shashith.darshana@pearson.com>
2017-06-01 19:39:43 +0530

    get user details api

---
+++ routes/api.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var db = require('./DBConnection');
+var assert = require('assert');
+var fs = require('fs');
+var busboy = require('connect-busboy');
+var mongodb = require('mongodb');
+
+exports.uploadFiles = function(req, res) {
+    appLog.info("File uploaded by " + req.user.name);
+    var fstream;
+    req.pipe(req.busboy);
+    req.busboy.on('file', function(fieldname, file, filename) {
+        filename = (new Date().getTime()).toString() + filename;
+        console.log("Uploading: " + filename);
+        fstream = fs.createWriteStream('public/assets/img/profile/' + filename);
+        file.pipe(fstream);
+        fstream.on('close', function() {
+            res.send({
+                "path": 'public/assets/img/profile/' + filename
+            });
+        });
+    });
+}
+
+exports.getUser = function(req, res) {
+    var email = (req.user.email).toLowerCase();
+    var adminList = config.admin;
+    console.log(email);
+    console.log(adminList);
+    if (adminList.indexOf(email) > -1) {
+        req.user.admin = true;
+    }
+    res.send(req.user);
+}
