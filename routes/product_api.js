0d3fb9d56f55d0b51171d47d8b4ac5b31bd925e9
Gunarathne <UGUNAPA@PEROOT.COM>
2017-06-21 17:50:25 +0530

    return only program list

--- routes/product_api.js
+++ routes/product_api.js
@@ -3,6 +3,6 @@ var esDriver = require('../esDriver.js');
 exports.getPrograms = function (req, res) {
     console.log('program api');
     esDriver.allPrograms(function (resultJson) {
-        res.json(resultJson.hits.hits);
+        res.json(resultJson.hits.hits[0]._source);
     });
 };
