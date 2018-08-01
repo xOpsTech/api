49b7a7ed9096a9aa35153a938ed6a2d3ac0d0622
Keminda Samaraweera <kemindasamaraweera@Kemindas-MacBook-Pro.local>
2018-08-01 21:18:21 +0530

    node change for jwt token verification changes

--- routes/Router.js
+++ routes/Router.js
@@ -4,25 +4,37 @@ var alertApi = require("./alert_api");
 var metricsApi = require("./metrics_api");
 var programApi = require("./product_api");
 var servicenow = require("./servicenow_api")
-var healthConfigs = require("./health_configs")
-var techConfigs = require("./tech")
+var healthConfigs = require("./health_configs");
+var techConfigs = require("./tech");
+var detailConfigs = require('./details');
+
 
 var router = require('express').Router();
+
+//GET METHODS
+
 router.route('/user').get(api.getUser);
-router.route('/gettenant/:tenant').get(api.getTenantIDbytenant); 
 router.route('/user/_list').get(api.getUserList);
+router.route('/user/:userId').get(api.getDbUser);
+router.route('/users/:tenantId').get(api.getUserByTenantId);
+
+router.route('/gettenant/:tenant').get(api.getTenantIDbytenant); 
+router.route('/userType/:tenantId').get(api.getUserTypeByTenantId)
+router.route('/tenant/:userId').get(api.getTenantByUserId); 
+router.route('/checkuser/:userId').get(api.checkuser);
+  
+router.route('/widget').get(api.getAllWidgets);
+
+
 router.route('/user').post(api.saveUser);
 router.route('/userType').post(api.saveUserType);
-router.route('/userType/:tenantId').get(api.getUserTypeByTenantId)
 router.route('/tenant').post(api.saveTenant);   
-router.route('/tenant/:userId').get(api.getTenantByUserId); 
-router.route('/users/:tenantId').get(api.getUserByTenantId);  
+
+
 router.route('/tenant/:tenantId').put(api.updateTenant);  
 router.route('/user/:userId').put(api.updateUser);
 router.route('userType/:name').put(api.updateUserType);
-router.route('/user/:userId').get(api.getDbUser);
-router.route('/checkuser/:userId').get(api.checkuser);
-router.route('/widget').get(api.getAllWidgets);
+
 router.route('/upload').post(api.uploadFiles);
 
 router.route('/scholastic/services-health').get(api.getServiceHealth);
---
+++ routes/app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var cors = require('cors')
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(cors())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+var userApi = require('./routes/project.js');
+var testApi = require('./routes/tests.js');
+
+app.use('/project', userApi);
+app.use('/test', testApi);
+
+app.listen(4000, function () {
+  console.log('app listening on port 4000!')
+});
